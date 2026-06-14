e284c6a3b68939a5f87d8b3266bfa3643eb13f64
Comatine <Comatine00@Gmail.com>
2026-06-14 18:01:37 +0900

    쓸모 없어진 자체 제작 라이브러리 재거

--- DuckDB/__init__.py
+++ DuckDB/__init__.py
@@ -1,13 +1,9 @@
 from .connection import get_connection
 from .item import Item
-from .map import get_location_by_id
-from .monster import get_monster_by_id
 from .user import User
 
 __all__ = [
     "get_connection",
     "Item",
-    "get_location_by_id",
-    "get_monster_by_id",
     "User"
 ]
--- DuckDB/map.py
+++
@@ -1,8 +0,0 @@
-from .connection import get_connection
-
-def get_location_by_id(location_id: int):
-    conn = get_connection()
-    query = "SELECT Location_Id, Location_Type, Location_Name, Location_Level FROM Location WHERE Location_Id = ?"
-    result = conn.execute(query, [location_id]).fetchall()
-    conn.close()
-    return result
